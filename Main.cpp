d47c770245f57d40bcf5353f477246969ec5df02
Igor <zakharov_igor@inbox.ru>
2021-10-11 22:34:21 +0300

    Added class MyList, Node(nested), default constructors, push_front method and push_back method

--- Main.cpp
+++ Main.cpp
@@ -4,5 +4,11 @@
 
 int main()
 {
+    
+    MyList<int> testList;
+    testList.push_back(5);
+    testList.push_front(32);
+    
+    
     std::cout << "Hello World!\n";
 }
--- MyList.h
+++ MyList.h
@@ -1,6 +1,51 @@
 #pragma once
-class MyList
-{
+template <typename T> class MyList {
+
+	template <typename T> class Node {
+	
+		friend class MyList<T>;
+		Node<T>* prev_, * next_;
+		T* data_;
+		Node() : prev_(nullptr), next_(nullptr), data_(nullptr) {};
+		explicit Node(const T& t) : prev_(nullptr), next_(nullptr) { data_ = new T(t); };
+		//~Node();
+	};
+
+		Node<T> head_, tail_;
+		size_t size_;
+		
+public:
+	MyList();
+	void push_front(const T&);
+	void push_back(const T&);
+
+
+	
+};
+
+template <typename T> MyList<T>::MyList() : size_( 0 ) {
+
+	head_.next_ = &tail_;
+	tail_.prev_ = &head_;
 
 };
 
+template <typename T> void MyList<T>::push_back(const T& t) {
+	
+	Node<T>* node{ new Node<T>(t) };
+	this->tail_.prev_->next_ = node;
+	node->prev_ = this->tail_.prev_;
+	node->next_ = &this->tail_;
+	this->tail_.prev_ = node;
+	++size_;
+}
+
+template <typename T> void MyList<T>::push_front(const T& t) {
+
+	Node<T>* node{ new Node<T>(t) };
+	this->head_.next_->prev_ = node;
+	node->next_ = this->head_.next_;
+	node->prev_ = &this->head_;
+	this->head_.next_ = node;
+	++size_;
+}
