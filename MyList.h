edd83c8fc75718ecc311086483b38bc49a76f6e5
Igor <zakharov_igor@inbox.ru>
2021-10-13 23:04:43 +0300

    Added: pop_front, pop_back

--- MyList.h
+++ MyList.h
@@ -1,27 +1,40 @@
 #pragma once
 template <typename T> class MyList {
 
-	template <typename T> class Node {
+	class Node {
 	
 		friend class MyList<T>;
-		Node<T>* prev_, * next_;
+		Node* prev_, * next_;
 		T* data_;
 		Node() : prev_(nullptr), next_(nullptr), data_(nullptr) {};
-		explicit Node(const T& t) : prev_(nullptr), next_(nullptr) { data_ = new T(t); };
-		//~Node();
+		explicit Node(const T& t);
+		~Node() { delete data_; };
 	};
 
-		Node<T> head_, tail_;
+		Node head_, tail_;
 		size_t size_;
 		
 public:
 	MyList();
 	void push_front(const T&);
 	void push_back(const T&);
+	void pop_front();
+	void pop_back();
 
 
 	
 };
+
+//Intellisense doesn't work propertly with templates
+//Instantinate template class with T = int
+
+
+template <typename T> MyList<T>::Node::Node(const T& t) : prev_(nullptr), next_(nullptr) {
+
+	data_ = new T(t); 
+
+};
+
 
 template <typename T> MyList<T>::MyList() : size_( 0 ) {
 
@@ -32,20 +45,45 @@ template <typename T> MyList<T>::MyList() : size_( 0 ) {
 
 template <typename T> void MyList<T>::push_back(const T& t) {
 	
-	Node<T>* node{ new Node<T>(t) };
-	this->tail_.prev_->next_ = node;
-	node->prev_ = this->tail_.prev_;
-	node->next_ = &this->tail_;
-	this->tail_.prev_ = node;
-	++size_;
+		Node* node{ new Node(t) };
+		this->tail_.prev_->next_ = node;
+		node->prev_ = this->tail_.prev_;
+		node->next_ = &this->tail_;
+		this->tail_.prev_ = node;
+		++size_;
+		
 }
 
 template <typename T> void MyList<T>::push_front(const T& t) {
 
-	Node<T>* node{ new Node<T>(t) };
+	Node* node{ new Node(t) };
 	this->head_.next_->prev_ = node;
 	node->next_ = this->head_.next_;
 	node->prev_ = &this->head_;
 	this->head_.next_ = node;
 	++size_;
+}
+
+template <typename T> void MyList<T>::pop_front() {
+
+	if (this->head_.next_ != &this->tail_)
+	{
+		Node* tmp = this->head_.next_;
+		this->head_.next_ = this->head_.next_->next_;
+		this->head_.next_->prev_ = &this->head_;
+		delete tmp;
+		
+	}
+		
+}
+
+template <typename T> void MyList<T>::pop_back() {
+
+	if (this->head_.next_ != &this->tail_)
+	{
+		Node* tmp = this->tail_.prev_;
+		this->tail_.prev_ = this->tail_.prev_->prev_;
+		this->tail_.prev_->next_ = &this->tail_;
+		delete tmp;
+	}
 }
